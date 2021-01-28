f6aecd3564ca4afcf205c4354da7a876db87fe66
Alex Parker <misterparker@gmail.com>
2021-01-27 18:58:30 -0700

    Add faker and seed data

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+#
+Enum.map(0..50, fn i ->
+  Homework.Repo.insert!(%Homework.Users.User{
+    first_name: Faker.Person.first_name(),
+    last_name: Faker.Person.last_name(),
+    dob: "#{Faker.Date.date_of_birth(18..98)}"
+  });
+end)
